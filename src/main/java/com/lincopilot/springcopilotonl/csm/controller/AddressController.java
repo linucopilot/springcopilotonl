99709c3707491717179292e1af3b85c8213ac3df
Koy Dalin <koydalin.km@gmail.com>
2026-08-05 13:34:18 +0700

    add docker deployment

--- src/main/java/com/lincopilot/springcopilotonl/csm/controller/AddressController.java
+++ src/main/java/com/lincopilot/springcopilotonl/csm/controller/AddressController.java
@@ -4,18 +4,25 @@ import com.lincopilot.springcopilotonl.common.constants.Constants;
 import com.lincopilot.springcopilotonl.common.dto.ResultPayload;
 import com.lincopilot.springcopilotonl.csm.service.AddressService;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/csm")
 public class AddressController {
-    AddressService addressService;
 
-     public ResultPayload login(HttpServletRequest request, @RequestBody Map<String, Object> inData) {
+    @Autowired
+    private AddressService addressService;
 
-        // log.info("AddressController.login() inData : {}", inData);
+    @PostMapping("/address")
+    public ResultPayload getAddress(HttpServletRequest request, @RequestBody Map<String, Object> inData) {
+
+        // log.info("AddressController.getAddress() inData : {}", inData);
         ResultPayload result = new ResultPayload();
         result.setResultCd(Constants.SUCCESS_CD);
         result.setResultData(addressService.selectAddress(request, inData));
--- src/main/java/com/lincopilot/springcopilotonl/csm/controller/LoginController.java
+++ src/main/java/com/lincopilot/springcopilotonl/csm/controller/LoginController.java
@@ -2,7 +2,11 @@ package com.lincopilot.springcopilotonl.csm.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lincopilot.springcopilotonl.common.constants.Constants;
@@ -12,10 +16,13 @@ import com.lincopilot.springcopilotonl.csm.service.LoginService;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
+@RequestMapping("/api/csm")
 public class LoginController {
 
-    LoginService loginService;
+    @Autowired
+    private LoginService loginService;
 
+    @PostMapping("/login")
     public ResultPayload login(HttpServletRequest request, @RequestBody Map<String, Object> inData) {
 
         // log.info("LoginController.login() inData : {}", inData);
@@ -23,9 +30,12 @@ public class LoginController {
         result.setResultCd(Constants.SUCCESS_CD);
         result.setResultData(loginService.actionLogin(request, inData));
         return result;
+    }
 
-        // coommit 1111
-        // coommit 2222
-        // coommit 3333
+    @GetMapping("/logout")
+    public ResultPayload logout(HttpServletRequest request) {
+        ResultPayload result = new ResultPayload();
+        result.setResultCd(Constants.SUCCESS_CD);
+        return result;
     }
 }
